04a60c29a65fd932992de021c6f58e4f6c87c9c0
jungr-ait <roland.jung@ait.ac.at>
2017-11-29 12:48:25 +0100

    Create setup_multiple_gcc.sh

---
+++ setup_multiple_gcc.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+echo "adding GCC 6.3.0, 7.2.0 on Ubuntu 16.04:"
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install build-essential software-properties-common -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+
+sudo apt-get update -y
+sudo apt-get install gcc-6 g++-6 -y
+sudo apt-get install gcc-7 g++-7 -y
+
+# default version is gcc 5.x
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 100 --slave /usr/bin/g++ g++ /usr/bin/g++-5
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 90 --slave /usr/bin/g++ g++ /usr/bin/g++-6
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 80 --slave /usr/bin/g++ g++ /usr/bin/g++-7
+
+sudo update-alternatives --config gcc
+gcc --version
